cf172c9b67b70297454ab272230c327119ebb9cc
Hilton <AMH0831@daugherty.com>
2019-10-02 10:36:55 -0500

    this commit is too big

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const beerRouter = require('./routes/beerRouter');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.use('/beers', beerRouter);
@@ -11,6 +14,19 @@ app.use('/', (req, res)=>{
 });
 
 const port = process.env.PORT || 4444
+
+mongoose.connect('mongodb://localhost:27017/beers', {
+    useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to the beers database')
+});
+
+
+mongoose.connection.on('error', () => {
+    console.log('error connecting to the beers database');
+});
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}...`);
