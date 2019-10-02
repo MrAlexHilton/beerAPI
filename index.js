bcab93f7c386a025462aeea23912a2fde80e6b75
Hilton <AMH0831@daugherty.com>
2019-10-02 09:37:15 -0500

    Added beer router and imported mongoose

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const beerRouter = require('./routes/beerRouter');
+const mongoose = require('mongoose');
+
+
+app.use('/beers', beerRouter);
 
 app.use('/', (req, res)=>{
-    console.log('Something hit me!');
-    res.send(`<h1>Welcome to the beer API</h1>`);
+        res.send(`<h1>Welcome to the beer API</h1>`);
 });
 
 const port = process.env.PORT || 4444
